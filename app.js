15da7925a52ee388078cc0d8ebe6966f6a937367
denver daniels <denver@projectcodex.co>
2016-04-23 22:39:30 +0200

    updated

--- app.js
+++ app.js
@@ -8,27 +8,11 @@ const express = require('express'),
     Wheel = require('./wheels'),
     Robot = require('./robot'),
     say = require('say'),
-    express = require('express'),
-    mysql = require('mysql'),
-    myConnection = require('express-myconnection'),
-    locations = require('./locations'),
-    join = require('./join'),
-    http = require('http'),
-    socket_io = require('socket.io'),
-    DataService = require('./data-service'),
-    api = require('./api'),
-    connectionProvider = require('connection-provider');
-    board = new five.Board({
-        port: '/dev/tty.CAPEBOT2-DevB'
-    });
+    // board = new five.Board({
+    //     port: '/dev/tty.CAPEBOT2-DevB'
+    // });
+    board = new five.Board();
 
-    var dbOptions = {
-          host: 'localhost',
-          user: 'geo',
-          password: 'password',
-          port: 3306,
-          database: 'geolocation'
-    };
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -40,25 +24,6 @@ app.engine('hbs', expressHandlebars({
 }));
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
-app.use(connectionProvider(dbOptions, function(connection){
-    return {
-        dataService : new DataService(connection)
-    }
-}));
-app.get('/', function(req,res){
-  res.render('index');
-});
-app.get('/locations/add', locations.showAdd);
-app.get('/locations/delete/:locationId', locations.delete);
-app.post('/locations/add', locations.add);
-app.get('/join/<location_id>', join.home);
-
-app.get('/api/distance/:from/:to', api.distance_from);
-app.get('/api/center', api.center);
-app.get('/api/in_circle/:from/:to/:distance', api.distance);
-app.get('/api/nearest/:from', api.nearest);
-app.get('/api/selected/:id', api.get_all);
-app.get('/api/locations', api.locations);
 
 board.on("ready", function() {
 
@@ -76,40 +41,32 @@ board.on("ready", function() {
             robot.forward();
             say.speak('Forward', 'Alex', 1);
         });
-    });
 
-    io.sockets.on('connection', function(socket) {
         socket.on('click1', function() {
             console.log('backward');
             robot.reverse();
             say.speak('Reverse', 'Alex', 1);
 
         });
-    });
 
-    io.sockets.on('connection', function(socket) {
         socket.on('click2', function() {
             console.log('Left');
             say.speak('Left', 'Alex', 1);
             robot.left();
         });
-    });
 
-    io.sockets.on('connection', function(socket) {
         socket.on('click3', function() {
-          say.speak('Right', 'Alex', 1);
+            say.speak('Right', 'Alex', 1);
             console.log('Right');
             robot.right();
         });
-    });
 
-    io.sockets.on('connection', function(socket) {
-        socket.on('click4', function() {
-          say.speak('stopping', 'Alex', 1);
-            console.log('stopping');
-            robot.stop();
-        });
+    socket.on('click4', function() {
+        say.speak('Stop', 'Alex', 1);
+        console.log('stopping');
+        robot.stop();
     });
+  });
 
 });
 
