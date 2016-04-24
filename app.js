b2274a870008e080718e85e0ac48e6e51e175333
denver daniels <denver@projectcodex.co>
2016-04-24 03:52:41 +0200

    updated app

---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require('express'),
+    expressHandlebars = require('express-handlebars'),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    bodyParser = require('body-parser'),
+    five = require('johnny-five'),
+    Wheel = require('./wheels'),
+    Robot = require('./robot'),
+    say = require('say'),
+    // board = new five.Board({
+    //     port: '/dev/tty.CAPEBOT2-DevB'
+    // });
+    board = new five.Board();
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
+app.use(express.static('public'));
+app.engine('hbs', expressHandlebars({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+board.on("ready", function() {
+    var ping = new five.Ping(4);
+    var wheel1 = new Wheel(9, 8, 180),
+        wheel2 = new Wheel(6, 7, 180);
+        const temperature = new five.Thermometer({
+          controller: 'TMP36',
+          pin: 'A0',
+          freq:600
+        });
+
+
+    var robot = new Robot(wheel1, wheel2);
+    this.repl.inject({
+        robot: robot
+    });
+
+    io.sockets.on('connection', function(socket) {
+        socket.on('click', function() {
+            console.log('Forward');
+            robot.forward();
+            say.speak('Forward', 'Alex', 1);
+        });
+
+        socket.on('click1', function() {
+            console.log('backward');
+            robot.reverse();
+            say.speak('Reverse', 'Alex', 1);
+
+        });
+
+        socket.on('click2', function() {
+            console.log('Left');
+            say.speak('Left', 'Alex', 1);
+            robot.left();
+        });
+
+        socket.on('click3', function() {
+            say.speak('Right', 'Alex', 1);
+            console.log('Right');
+            robot.right();
+        });
+
+    socket.on('click4', function() {
+        say.speak('Stop', 'Alex', 1);
+        console.log('stopping');
+        robot.stop();
+    });
+  temperature.on('data', function() {
+    // console.log(this.celsius);
+    var temp = this.celsius;
+  socket.emit('temp',{temp:temp})
+});
+ping.on('change',function(value){
+  console.log(value);
+  console.log(this.in);
+  if(this.in > 2 && this.in < 3){
+            robot.stop();
+            var alert = 'Object, detected stopping now';
+          socket.emit('alert',{alert:alert})
+        }
+});
+});
+});
+
+var port = process.env.port || 3007;
+http.listen(port, function() {
+    console.log('running at port :', port)
+});
